e5f85382595bd7177517dd10c464e0d9cce042ba
urroboros <gu.oleg1@gmail.com>
2017-10-03 15:28:31 +0300

    add spaces for show global report

--- app/src/main/java/com/example/reports/ShowReportActivity.java
+++ app/src/main/java/com/example/reports/ShowReportActivity.java
@@ -46,12 +46,11 @@ public class ShowReportActivity extends AppCompatActivity {
             sizeStudyingBible += nameAndUserEntry.getValue().get(MainActivity.KEY_FOR_DB_SIZE_STUDYING_BIBLE);
         }
         Log.d(MainActivity.TAG, "names :     " + names);
-        ((TextView) findViewById(R.id.et_size_publication_show_report)).append(String.valueOf(sizePublications));
-        ((TextView) findViewById(R.id.et_size_videos_show_report)).append(String.valueOf(sizeVideos));
-        ((TextView) findViewById(R.id.et_size_hours_show_report)).append(String.valueOf(sizeHours));
-        ((TextView) findViewById(R.id.et_size_repeated_visits_show_report)).append(String.valueOf(sizeRepeatVisits));
-        ((TextView) findViewById(R.id.et_size_studying_bible_show_report)).append(String.valueOf(sizeStudyingBible));
+        ((TextView) findViewById(R.id.et_size_publication_show_report)).append(":  " + String.valueOf(sizePublications));
+        ((TextView) findViewById(R.id.et_size_videos_show_report)).append(":  " + String.valueOf(sizeVideos));
+        ((TextView) findViewById(R.id.et_size_hours_show_report)).append(":  " + String.valueOf(sizeHours));
+        ((TextView) findViewById(R.id.et_size_repeated_visits_show_report)).append(":  " + String.valueOf(sizeRepeatVisits));
+        ((TextView) findViewById(R.id.et_size_studying_bible_show_report)).append(":  " + String.valueOf(sizeStudyingBible));
         listViewAllUsersWhoSendReport.setAdapter(new ArrayAdapter<>(this, R.layout.item_simple_name, R.id.TextView, names.toArray(new String[names.size()])));
-
     }
 }
